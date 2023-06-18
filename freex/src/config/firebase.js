f66c40347d09f5ccff0a157bfbc86a36710d33a5
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-06-18 12:49:14 +0200

    Create project template

---
+++ freex/src/config/firebase.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { firebaseConfig } from './firebase.config';
+
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
