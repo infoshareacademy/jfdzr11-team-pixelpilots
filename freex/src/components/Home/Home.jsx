8f77f663721d8ae9fd938a90c10924dbe6413b87
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-06-21 17:08:53 +0200

    Add styling and improve Navbar

--- freex/src/components/Home/Home.jsx
+++ freex/src/components/Home/Home.jsx
@@ -1,13 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import useAuth from "../Context/AuthContext";
 
 const Home = () => {
   const navigate = useNavigate();
+  const { currentUser } = useAuth();
+
   return (
     <div>
-      <h2>Home</h2>
-      <button onClick={() => navigate("/login")}>Zaloguj się</button>
-      <p>Nie masz jeszcze konta?</p>
-      <button onClick={() => navigate("/register")}>Zarejestruj się</button>
+      {currentUser ? (
+        <h2>Home</h2>
+      ) : (
+        <>
+          <button onClick={() => navigate("/login")}>Zaloguj się</button>
+          <p>Nie masz jeszcze konta?</p>
+          <button onClick={() => navigate("/register")}>Zarejestruj się</button>
+        </>
+      )}
     </div>
   );
 };
--- freex/src/components/Navbar/Navbar.jsx
+++ freex/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import styles from "./Navbar.module.css";
 import NavbarLink from "../UI/NavbarLink/NavbarLink";
 import useAuth from "../Context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { currentUser, logout } = useAuth();
@@ -9,31 +9,77 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.links}>
-        {!currentUser ? (
-          <NavbarLink linkURL="/">Home</NavbarLink>
-        ) : (
-          <>
-            <NavbarLink linkURL="/userID/panelglowny">Panel główny</NavbarLink>
-            <NavbarLink linkURL="/userID/mojeoferty">Moje oferty</NavbarLink>
-            <NavbarLink linkURL="/userID/dodajoferte">Dodaj ofertę</NavbarLink>
-            <NavbarLink linkURL="/userID/mojezlecenia">
-              Moje zlecenia
-            </NavbarLink>
-            <NavbarLink linkURL="/userID/znajdzzlecenie">
-              Znajdź zlecenie
-            </NavbarLink>
-            <button
-              onClick={() => {
-                logout();
-                navigate("/");
-              }}
-            >
-              Logout
-            </button>
-          </>
-        )}
+      <div className={styles.wrapper}>
+        <div className={styles.navbar_left}>
+          <NavLink className={styles.logo_link} to="/">
+            <img className={styles.logo} src="../../../Navbar/freex_logo.png" />
+          </NavLink>
+          {!currentUser ? null : (
+            <>
+              <NavbarLink linkURL="/userID/panelglowny">
+                Panel główny
+              </NavbarLink>
+              <NavbarLink linkURL="/userID/mojeoferty">Moje oferty</NavbarLink>
+              <NavbarLink linkURL="/userID/dodajoferte">
+                Dodaj ofertę
+              </NavbarLink>
+              <NavbarLink linkURL="/userID/mojezlecenia">
+                Moje zlecenia
+              </NavbarLink>
+              <NavbarLink linkURL="/userID/znajdzzlecenie">
+                Znajdź zlecenie
+              </NavbarLink>
+            </>
+          )}
+        </div>
+        <div className={styles.navbar_right}>
+          {currentUser ? (
+            <>
+              <button
+                className={styles.button_logout}
+                onClick={() => {
+                  logout();
+                  navigate("/");
+                }}
+              >
+                Wyloguj się
+              </button>
+              <button className={styles.button_icon}>
+                <img
+                  className={styles.icon}
+                  src="../../../Navbar/icon_ulubione.png"
+                />
+              </button>
+              <button className={styles.button_icon}>
+                <img
+                  className={styles.icon}
+                  src="../../../Navbar/icon_profil.png"
+                />
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className={styles.button_logout}
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                Zaloguj się
+              </button>
+              <button
+                className={styles.button_logout}
+                onClick={() => {
+                  navigate("/register");
+                }}
+              >
+                Zarejetruj się
+              </button>
+            </>
+          )}
+        </div>
       </div>
+      <div className={styles.underline}></div>
     </nav>
   );
 };
