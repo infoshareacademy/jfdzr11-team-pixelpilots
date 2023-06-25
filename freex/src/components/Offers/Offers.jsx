0b4dc8669d1567b20283b15364f8aa51bcfd8c0d
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-06-25 13:06:26 +0200

    Create an Offers component that displays all offers

--- freex/src/components/Offers/Offers.jsx
+++ freex/src/components/Offers/Offers.jsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react';
+import { db } from '../../config/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import styles from './Offers.module.css';
+
 const Offers = () => {
-  return <div>Offers</div>;
+	const [offers, setOffers] = useState([]);
+	const offersCollectionRef = collection(db, 'offers');
+
+	const getOffers = async () => {
+		try {
+			const data = await getDocs(offersCollectionRef);
+			const filteredData = data.docs.map((doc) => ({
+				id: doc.id,
+				...doc.data(),
+			}));
+			setOffers(filteredData);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getOffers();
+	}, []);
+
+	return (
+		<div className={styles.wrapper}>
+			<h2>Oferty:</h2>
+			{offers
+				? offers.map((offer) => (
+						<div key={offer.id} className={styles.card}>
+							<h4>{offer.title}</h4>
+							<p>{offer.description}</p>
+							<strong>{offer.hourly_rate}</strong>
+							<ul>
+								{offer.skills.map((skill) => (
+									<li key={skill}>{skill}</li>
+								))}
+							</ul>
+						</div>
+				  ))
+				: null}
+		</div>
+	);
 };
 
 export default Offers;
