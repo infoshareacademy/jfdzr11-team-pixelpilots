02f0b4bff2c2769875343c09019226776e9ee3bb
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-06-29 11:51:41 +0200

    feat: add filter functionality in the Offers component

--- freex/src/components/Offers/Offers.jsx
+++ freex/src/components/Offers/Offers.jsx
@@ -1,32 +1,84 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { db } from '../../config/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import {
+	collection,
+	getDocs,
+	onSnapshot,
+	query,
+	where,
+} from 'firebase/firestore';
 import styles from './Offers.module.css';
+import { Link } from 'react-router-dom';
+
+const skills = [
+	'Projektowanie graficzne',
+	'UI / User Interface',
+	'UX / User Experience',
+	'UX research',
+	'UX writing',
+	'Tworzenie logo',
+	'Responsywne projektowanie',
+	'Tworzenie stron internetowych',
+	'Aplikacja mobilna',
+	'Aplikacja webowa',
+	'Photoshop',
+	'Figma',
+	'Adobe XD',
+];
 
 const Offers = () => {
 	const [offers, setOffers] = useState([]);
 	const offersCollectionRef = collection(db, 'offers');
 
-	const getOffers = async () => {
-		try {
-			const data = await getDocs(offersCollectionRef);
-			const filteredData = data.docs.map((doc) => ({
-				id: doc.id,
-				...doc.data(),
-			}));
-			setOffers(filteredData);
-		} catch (error) {
-			console.error(error);
-		}
+	const getOffers = (querySnapshot) => {
+		return querySnapshot.docs.map((doc) => ({
+			id: doc.id,
+			...doc.data(),
+		}));
+	};
+
+	const filteredOffers = async (searchTerm) => {
+		const q = searchTerm
+			? query(
+					offersCollectionRef,
+					where('skills', 'array-contains', searchTerm)
+			  )
+			: offersCollectionRef;
+		getDocs(q).then((querySnapshot) => setOffers(getOffers(querySnapshot)));
 	};
 
 	useEffect(() => {
-		getOffers();
+		onSnapshot(offersCollectionRef, (querySnapshot) => {
+			const offers = getOffers(querySnapshot);
+			setOffers(offers);
+		});
 	}, []);
 
 	return (
 		<div className={styles.wrapper}>
 			<h2>Oferty:</h2>
+			<form className={styles.form}>
+				<input
+					type="radio"
+					name="skill"
+					id="all"
+					value=""
+					onChange={(e) => filteredOffers(e.target.value)}
+				/>
+				<label htmlFor="all">Wyświetl wszystkie</label>
+				{skills.map((skill) => (
+					<Fragment key={skill}>
+						<input
+							type="radio"
+							name="skill"
+							id={skill}
+							value={skill}
+							onChange={(e) => filteredOffers(e.target.value)}
+						/>
+						<label htmlFor={skill}>{skill}</label>
+					</Fragment>
+				))}
+			</form>
 			{offers
 				? offers.map((offer) => (
 						<div key={offer.id} className={styles.card}>
@@ -38,6 +90,10 @@ const Offers = () => {
 									<li key={skill}>{skill}</li>
 								))}
 							</ul>
+
+							<Link to={`/zlecenia/zlecenie/${offer.id}`}>
+								Szczegóły
+							</Link>
 						</div>
 				  ))
 				: null}
