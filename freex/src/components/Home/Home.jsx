ebee302d97dc6ebb79498639e42580f43fbc407d
Damian Smigielski <damsmigielski@gmail.com>
2023-07-01 12:53:45 +0200

    add home styles

--- freex/src/components/Home/Home.jsx
+++ freex/src/components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import styles from "../Home/Home.module.css";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../Context/AuthContext";
 
@@ -6,14 +7,37 @@ const Home = () => {
   const { currentUser } = useAuth();
 
   return (
-    <div>
+    <div className={styles.container}>
       {currentUser ? (
-        <h2>Home</h2>
+        <>{/* <h1>Witaj ponownie!</h1> */}</>
       ) : (
+        //powrót do strony głównej//
         <>
-          <button onClick={() => navigate("/login")}>Zaloguj się</button>
-          <p>Nie masz jeszcze konta?</p>
-          <button onClick={() => navigate("/register")}>Zarejestruj się</button>
+          <div className={styles.left}>
+            <h1>
+              Załóż konto już dziś i dołącz do społeczności profesjonalistów
+            </h1>
+            <p>
+              Nieważne, czy jesteś doświadczonym freelancerem, który szuka
+              nowych możliwości i projektów, czy przedsiębiorcą dążącym do
+              rozwoju swojej firmy - tutaj znajdziesz wsparcie, inspirację i
+              narzędzia, które pomogą Ci osiągnąć Twoje cele!
+            </p>
+          </div>
+          <div className={styles.right}>
+            <button
+              className={styles.homebtn}
+              onClick={() => navigate("/login")}>
+              Zaloguj się
+            </button>
+            <img src="../Home/searching.png"></img>
+            <button
+              className={styles.homebtn}
+              onClick={() => navigate("/register")}>
+              Zarejestruj się
+            </button>
+            <img src="../Home/designer.png"></img>
+          </div>
         </>
       )}
     </div>
