9f757cc48c7d6bebe702c1c32cf7b1a6ac124275
Damian Smigielski <damsmigielski@gmail.com>
2023-07-01 12:53:20 +0200

    add register styles

--- freex/src/components/Auth/Register/Register.jsx
+++ freex/src/components/Auth/Register/Register.jsx
@@ -1,66 +1,79 @@
-import styles from './Register.module.css';
-import useAuth from '../../Context/AuthContext';
-import { toast } from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
-import { firebaseErrors } from '../../../utils/firebaseErrors';
+import styles from "./Register.module.css";
+import useAuth from "../../Context/AuthContext";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { firebaseErrors } from "../../../utils/firebaseErrors";
 
 const Register = () => {
-	const navigate = useNavigate();
-	const { register } = useAuth();
+  const navigate = useNavigate();
+  const { register } = useAuth();
 
-	const handleSubmit = async (formEvent) => {
-		formEvent.preventDefault();
+  const handleSubmit = async (formEvent) => {
+    formEvent.preventDefault();
 
-		const email = formEvent.target?.email.value;
-		const password = formEvent.target?.password.value;
-		const password_confirm = formEvent.target?.password_confirm.value;
+    const email = formEvent.target?.email.value;
+    const password = formEvent.target?.password.value;
+    const password_confirm = formEvent.target?.password_confirm.value;
 
-		if (password !== password_confirm) {
-			toast.error('Hasła nie są jednakowe');
-		} else {
-			try {
-				await register(email, password);
-				navigate('/userID/panelglowny');
-				toast.success('Rejestracja zakończona sukcesem');
-			} catch (error) {
-				{
-					firebaseErrors[error.code]
-						? toast.error(firebaseErrors[error.code])
-						: toast.error('Wystąpił błąd. Spróbuj później');
-				}
-			}
-			formEvent.target.reset();
-		}
-	};
+    if (password !== password_confirm) {
+      toast.error("Hasła nie są jednakowe");
+    } else {
+      try {
+        await register(email, password);
+        navigate("/userID/panelglowny");
+        toast.success("Rejestracja zakończona sukcesem");
+      } catch (error) {
+        {
+          firebaseErrors[error.code]
+            ? toast.error(firebaseErrors[error.code])
+            : toast.error("Wystąpił błąd. Spróbuj później");
+        }
+      }
+      formEvent.target.reset();
+    }
+  };
 
-	return (
-		<div className={styles.container}>
-			<form onSubmit={handleSubmit} className={styles.form}>
-				<label htmlFor="email">Adres email</label>
-				<input
-					type="email"
-					name="email"
-					id="email"
-					placeholder="Wpisz adres email..."
-				/>
-				<label htmlFor="password">Hasło</label>
-				<input
-					type="password"
-					name="password"
-					id="password"
-					placeholder="Wpisz hasło..."
-				/>
-				<label htmlFor="password_confirm">Potwierdź hasło</label>
-				<input
-					type="password"
-					name="password_confirm"
-					id="password_confirm"
-					placeholder="Potwierdź hasło..."
-				/>
-				<button type="submit">Zarejestruj się</button>
-			</form>
-		</div>
-	);
+  const handleResetPassword = async (email) => {
+    try {
+      await resetPassword(email);
+      toast.success("Wysłano wiadomość z resetem hasła");
+    } catch (error) {
+      if (firebaseErrors[error.code]) {
+        toast.error(firebaseErrors[error.code]);
+      } else {
+        toast.error("Wystąpił błąd. Spróbuj później");
+      }
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="email">Adres e-mail</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Wpisz adres email..."
+        />
+        <label htmlFor="password">Hasło</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Wpisz hasło..."
+        />
+        <label htmlFor="password_confirm">Potwierdź hasło</label>
+        <input
+          type="password"
+          name="password_confirm"
+          id="password_confirm"
+          placeholder="Potwierdź hasło..."
+        />
+        <button type="submit">Zarejestruj się</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
