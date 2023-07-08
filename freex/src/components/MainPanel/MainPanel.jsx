1179a52c241374ab0058b48be072d5ee56f57695
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-07-08 14:47:41 +0200

    feat: add Accordion component to MainPanel component

--- freex/src/components/MainPanel/MainPanel.jsx
+++ freex/src/components/MainPanel/MainPanel.jsx
@@ -1,12 +1,14 @@
-import { Cards } from "../index";
-import styles from "./MainPanel.module.css";
+import Accordion from './Accordion/Accordion';
+import { Cards } from '../index';
+import styles from './MainPanel.module.css';
 
 const MainPanel = () => {
-  return (
-    <div className={styles.wrapper}>
-      <Cards />
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<Cards />
+			<Accordion />
+		</div>
+	);
 };
 
 export default MainPanel;
