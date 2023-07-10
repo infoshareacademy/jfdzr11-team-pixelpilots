3ea97b39466e51a223f10d2bb27e0f4cb7f49449
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-07-10 10:47:14 +0200

    feat: add defaultChecked skill

--- freex/src/components/Offers/Skill/Skill.jsx
+++ freex/src/components/Offers/Skill/Skill.jsx
@@ -1,9 +1,19 @@
 import styles from './Skill.module.css';
 
-const Skill = ({ id, value, type, name, onChange, htmlFor, children }) => {
+const Skill = ({
+	id,
+	value,
+	type,
+	name,
+	onChange,
+	htmlFor,
+	children,
+	defaultChecked,
+}) => {
 	return (
 		<>
 			<input
+				defaultChecked={defaultChecked}
 				className={styles.input}
 				type={type}
 				name={name}
