d6e9ee4b13184881a8466833b0c70ca30a4b4996
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-07-15 14:17:10 +0200

    fix: change img source in Refister component

--- freex/src/components/Auth/Register/Register.jsx
+++ freex/src/components/Auth/Register/Register.jsx
@@ -1,83 +1,87 @@
-import styles from "./Register.module.css";
-import useAuth from "../../Context/AuthContext";
-import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
-import { firebaseErrors } from "../../../utils/firebaseErrors";
-import { PrimaryButton } from "../../index";
+import styles from './Register.module.css';
+import useAuth from '../../Context/AuthContext';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { firebaseErrors } from '../../../utils/firebaseErrors';
+import { PrimaryButton } from '../../index';
 
 const Register = () => {
-  const navigate = useNavigate();
-  const { register } = useAuth();
+	const navigate = useNavigate();
+	const { register } = useAuth();
 
-  const handleSubmit = async (formEvent) => {
-    formEvent.preventDefault();
+	const handleSubmit = async (formEvent) => {
+		formEvent.preventDefault();
 
-    const email = formEvent.target?.email.value.trim(" ");
-    const password = formEvent.target?.password.value.trim(" ");
-    const password_confirm = formEvent.target?.password_confirm.value.trim(" ");
+		const email = formEvent.target?.email.value.trim(' ');
+		const password = formEvent.target?.password.value.trim(' ');
+		const password_confirm =
+			formEvent.target?.password_confirm.value.trim(' ');
 
-    if (password !== password_confirm) {
-      toast.error("Hasła nie są jednakowe");
-    } else {
-      try {
-        await register(email, password);
-        navigate("/");
-        toast.success("Rejestracja zakończona sukcesem");
-      } catch (error) {
-        {
-          firebaseErrors[error.code]
-            ? toast.error(firebaseErrors[error.code])
-            : toast.error("Wystąpił błąd. Spróbuj później");
-        }
-      }
-      formEvent.target.reset();
-    }
-  };
+		if (password !== password_confirm) {
+			toast.error('Hasła nie są jednakowe');
+		} else {
+			try {
+				await register(email, password);
+				navigate('/');
+				toast.success('Rejestracja zakończona sukcesem');
+			} catch (error) {
+				{
+					firebaseErrors[error.code]
+						? toast.error(firebaseErrors[error.code])
+						: toast.error('Wystąpił błąd. Spróbuj później');
+				}
+			}
+			formEvent.target.reset();
+		}
+	};
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.text}>
-        <h2>Cieszymy się, że to właśnie z nami chcesz rozwijać swój biznes</h2>
-        <p>
-          Jeszcze tylko kilka kroków i będziesz mógł w pełni korzystać z
-          funkcjonalności FreeX. Uzupełnij podstawowe dane i znajdź specjalistów
-          na miarę Twoich potrzeb.
-        </p>
-        <div className={styles.image}>
-          <img
-            src="../../../../public/Register/register_hero.png"
-            alt="register icon"
-          />
-        </div>
-      </div>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div>
-          <label htmlFor="email">Adres email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Wpisz adres e-mail..."
-          />
-          <label htmlFor="password">Hasło</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Wpisz hasło..."
-          />
-          <label htmlFor="password_confirm">Potwierdź hasło</label>
-          <input
-            type="password"
-            name="password_confirm"
-            id="password_confirm"
-            placeholder="Potwierdź hasło..."
-          />
-          <PrimaryButton type="submit">Zarejestruj się</PrimaryButton>
-        </div>
-      </form>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.text}>
+				<h2>
+					Cieszymy się, że to właśnie z nami chcesz rozwijać swój
+					biznes
+				</h2>
+				<p>
+					Jeszcze tylko kilka kroków i będziesz mógł w pełni korzystać
+					z funkcjonalności FreeX. Uzupełnij podstawowe dane i znajdź
+					specjalistów na miarę Twoich potrzeb.
+				</p>
+				<div className={styles.image}>
+					<img
+						src="/Register/register_hero.png"
+						alt="register icon"
+					/>
+				</div>
+			</div>
+			<form onSubmit={handleSubmit} className={styles.form}>
+				<div>
+					<label htmlFor="email">Adres email</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						placeholder="Wpisz adres e-mail..."
+					/>
+					<label htmlFor="password">Hasło</label>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="Wpisz hasło..."
+					/>
+					<label htmlFor="password_confirm">Potwierdź hasło</label>
+					<input
+						type="password"
+						name="password_confirm"
+						id="password_confirm"
+						placeholder="Potwierdź hasło..."
+					/>
+					<PrimaryButton type="submit">Zarejestruj się</PrimaryButton>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Register;
