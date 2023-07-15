d3bf085b58cb187376389af1ba848e7bb30d17c4
Damian Smigielski <damsmigielski@gmail.com>
2023-07-15 13:40:51 +0200

    add - fix layoput of forgotpass

--- freex/src/components/Auth/ForgotPassword/ForgotPassword.jsx
+++ freex/src/components/Auth/ForgotPassword/ForgotPassword.jsx
@@ -6,26 +6,26 @@ import { PrimaryButton } from "../../index";
 import { toast } from "react-hot-toast";
 
 const Form = ({ submitText, isPasswordHidden = false, onSubmit }) => (
-  <form className={styles.form} onSubmit={onSubmit}>
-    <div>
-      <label htmlFor="email">
-        Podaj swój adres e-mail do przypomnienia hasła
-      </label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="Wpisz adres e-mail..."
-      />
+  <div className={styles.container}>
+    <div className={styles.left}>
+      <img className={styles.box} src="./Login/login_photo.png"></img>
     </div>
-    {!isPasswordHidden && (
-      <div>
-        <label htmlFor="password">Podaj hasło</label>
-        <input type="password" name="password" id="password" />
-      </div>
-    )}
-    <PrimaryButton>{submitText}</PrimaryButton>
-  </form>
+    <div className={styles.right}>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <div className={styles.forminnerdiv}>
+          <h2>Wpisz adres do przypomnienia hasła</h2>
+          <label htmlFor="email">Adres e-mail</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Wpisz adres e-mail..."
+          />
+          <PrimaryButton>{submitText}</PrimaryButton>
+        </div>
+      </form>
+    </div>
+  </div>
 );
 
 export const ForgotPassword = () => {
