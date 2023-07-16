555b8f3010ea0f1a83534fb6aa5c19b9d0475bdf
Jakub Meyza <kubameyza@gmail.com>
2023-07-16 08:41:15 +0200

    fix: fix bugs xD

--- freex/src/components/MainPanel/Carousel/Carousel.jsx
+++ freex/src/components/MainPanel/Carousel/Carousel.jsx
@@ -10,97 +10,95 @@ import { Link } from 'react-router-dom';
 import { CiCircleChevLeft, CiCircleChevRight } from 'react-icons/ci';
 
 const Carousel = () => {
-	const [users, setUsers] = useState([]);
-	const [sliderRef, setSliderRef] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [sliderRef, setSliderRef] = useState(null);
 
-	const collectionRef = collection(db, 'users');
+  const collectionRef = collection(db, 'users');
 
-	const settings = {
-		dots: false,
-		arrows: false,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		infinite: true,
-		speed: 500,
-	};
+  const settings = {
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 500,
+  };
 
-	const calculateAverageRating = (opinions) => {
-		const ratingsSum = opinions.reduce(
-			(accumulator, currentObject) =>
-				accumulator + Number(currentObject.rating),
-			0
-		);
-		const averageRating = ratingsSum / opinions.length;
-		return averageRating;
-	};
+  const calculateAverageRating = (opinions) => {
+    const ratingsSum = opinions?.reduce(
+      (accumulator, currentObject) =>
+        accumulator + Number(currentObject.rating),
+      0
+    );
+    const averageRating = ratingsSum / opinions?.length;
+    return averageRating;
+  };
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const querySnapshot = await getDocs(collectionRef);
-				const data = querySnapshot.docs.map((doc) => doc.data());
-				const sortedData = data.sort((a, b) => {
-					const ratingA = calculateAverageRating(a.opinions);
-					const ratingB = calculateAverageRating(b.opinions);
-					return ratingB - ratingA;
-				});
-				setUsers(sortedData);
-			} catch (error) {
-				toast.error(
-					'Pojawił się błąd. Spróbuj później. Error ' + error
-				);
-			}
-		};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collectionRef);
+        const data = querySnapshot.docs.map((doc) => doc.data());
+        const sortedData = data.sort((a, b) => {
+          const ratingA = calculateAverageRating(a.opinions);
+          const ratingB = calculateAverageRating(b.opinions);
+          return ratingB - ratingA;
+        });
+        setUsers(sortedData);
+      } catch (error) {
+        toast.error('Pojawił się błąd. Spróbuj później. Error ' + error);
+      }
+    };
 
-		fetchData();
-	}, []);
+    fetchData();
+  }, []);
 
-	return (
-		<>
-			<div className={styles.wrapper}>
-				<h3>Najbardziej polecani freelancerzy</h3>
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h3>Najbardziej polecani freelancerzy</h3>
 
-				<div className={styles.list}>
-					<div className={styles.controls}>
-						<button
-							onClick={sliderRef?.slickPrev}
-							className={styles.controlsPrev}
-						>
-							<CiCircleChevLeft />
-						</button>
-						<button
-							onClick={sliderRef?.slickNext}
-							className={styles.controlsNext}
-						>
-							<CiCircleChevRight />
-						</button>
-					</div>
-					<Slider ref={setSliderRef} {...settings}>
-						{users
-							.filter((user) => user.opinions.length > 0)
-							.map((user) => (
-								<Link
-									to={`/freelancerzy/${user.id}`}
-									key={nanoid()}
-									className={styles.link}
-								>
-									<UserCard
-										key={nanoid()}
-										role={user.role}
-										email={user.email}
-										src={user.imgURL}
-										name={user.userName}
-										opinions={user.opinions}
-										id={user.id}
-										rating={user.rating}
-									/>
-								</Link>
-							))}
-					</Slider>
-				</div>
-			</div>
-		</>
-	);
+        <div className={styles.list}>
+          <div className={styles.controls}>
+            <button
+              onClick={sliderRef?.slickPrev}
+              className={styles.controlsPrev}
+            >
+              <CiCircleChevLeft />
+            </button>
+            <button
+              onClick={sliderRef?.slickNext}
+              className={styles.controlsNext}
+            >
+              <CiCircleChevRight />
+            </button>
+          </div>
+          <Slider ref={setSliderRef} {...settings}>
+            {users
+              .filter((user) => user.opinions?.length > 0)
+              .map((user) => (
+                <Link
+                  to={`/freelancerzy/${user.id}`}
+                  key={nanoid()}
+                  className={styles.link}
+                >
+                  <UserCard
+                    key={nanoid()}
+                    role={user.role}
+                    email={user.email}
+                    src={user.imgURL}
+                    name={user.userName}
+                    opinions={user.opinions}
+                    id={user.id}
+                    rating={user.rating}
+                  />
+                </Link>
+              ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Carousel;
